57dcf8b56b92da4db75a5e502dfd0a131a3fecb8
Z1R0Axcel <113200832+Z1R0Axcel@users.noreply.github.com>
2022-11-11 14:15:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 int main() {
   int num;
-  cout << "Enter an integer between 1 and 1000: ";
-  cin >> num;
+ cin >> num;
   if (num % 2 == 0 && num !=2 || num % 3 == 0 && num !=3 || num % 5 == 0 && num !=5 || num % 8 == 0 && num !=8 ||
       num % 11 == 0 && num !=11 || num % 13 == 0 && num !=13 || num % 17 == 0 && num !=17 || num % 19 == 0 && num !=19 ||
       num % 23 == 0 && num !=23 || num % 29 == 0 && num !=29 || num % 31 == 0 && num !=31) {
